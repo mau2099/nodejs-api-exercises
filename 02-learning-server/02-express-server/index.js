d9c7905015bae8882f7f6eead91d18cb64e4b5de
Mauricio Garcia <fidel.garcia@globant.com>
2020-05-25 18:33:45 -0500

    express server sample

--- 02-learning-server/02-express-server/index.js
+++ 02-learning-server/02-express-server/index.js
@@ -8,6 +8,15 @@ app.get('/', function (req, res) {
   res.send('hello wordl');
 });
 app.get('/json', function (req, res) {
+  console.log(req.body);
+  res.json({ hello: 'world' });
+});
+app.get('/json/:name', function (req, res) {
+  // http://localhost:2080/json/2?otrder=asdsad&asdasd=123123213
+  // http://localhost:2080/json/2?shoe[color]=blue&shoe[size]=32
+  console.log("req body", req.body);
+  console.log("req params", req.params);
+  console.log("req query", req.query);
   res.json({ hello: 'world' });
 });
 
---
+++ 02-learning-server/README.md
@@ -0,0 +1,6 @@
+en este proyecto es de ejemplo para crear un servidor con node
+y posteriormente (02-express-server) es cuando se crea un ejemplo de un servidor en express
+
+___
+npm run dev
+npm run start
